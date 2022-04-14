19feebbfaa4e88c6ff02c6f51e6b4d3bc6318965
chuahx <xinyu.chuah@student.unimelb.edu.au>
2022-04-14 17:12:06 +1000

    Changed percentages to responses and added listener for connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,42 @@
 import './App.css';
 import { useState, useEffect } from "react";
-import io from 'socket.io-client';
+import { io } from 'socket.io-client';
 // import {Button} from "@mui/material";
 
 function App() {
-    const [percentages, setPercentages] = useState({});
+    const [responses, setResponses] = useState({});
     const [movieInput, setMovieInput] = useState('');
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
         // Create and establish a connection to the socketio server
-        const newSocket = io(`http://${window.location.hostname}:3000`);
+        const ENDPOINT = "http://localhost:8000";
+        const socket = io(ENDPOINT);
+        
+        if (socket) {
+            setSocket(socket);
+            
+            socket.on("connected", data => {
+                console.log("connected")
+                setResponses(data);
+            });
 
-        setSocket(newSocket);
-
-        socket.on("receive-respose", data => {
-            setPercentages(data);
-        })
-
-        return (
-            // close the connection when the component disappears from the DOM (aka clean up the effect)
-            newSocket.close()
-        )
-    }, [setSocket]);
+            socket.on("receive-response", data => {
+                console.log("receive-response")
+                setResponses(data);
+            });
+        }
+    }, [setResponses]);
 
     function handleSubmit(event) {
         event.preventDefault();
-        console.log("movieInput: " + movieInput);
-        socket.emit('send-response', movieInput);
+        socket.emit("send-response", movieInput);
         setMovieInput('');  // make input box empty
     }
 
     return (
         <div className="App">
-            {console.log("movies: " + JSON.stringify(percentages, null, 2))}
-            {/* {console.log("socket: " + JSON.stringify(socket, null, 2))} */}
-            <p>{socket!=null ? "Socket Connection Established" : "No connection"}</p>
+            {/* {console.log("movies: " + JSON.stringify(responses, null, 2))} */}
             <h1>Favourite Movie Poll</h1>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="colour">Enter your favourite movie:</label>
@@ -65,10 +66,10 @@ function App() {
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.keys(percentages).map((key, index) => (
-                        <tr>
+                    {Object.keys(responses).map((key, index) => (
+                        <tr key={key}>
                             <td>{key}</td>
-                            <td>{percentages[key]}</td>
+                            <td>{responses[key]}</td>
                         </tr>
                     ))}
                 </tbody>
