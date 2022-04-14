e4b7d028d643e5be1bce68c3f05a3499fc526172
chuahx <xinyu.chuah@student.unimelb.edu.au>
2022-04-14 10:53:40 +1000

    Added socketio to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,60 +1,77 @@
 import './App.css';
-import {useState} from "react";
+import { useState, useEffect } from "react";
+import io from 'socket.io-client';
 // import {Button} from "@mui/material";
 
 function App() {
-    // const [message, setMessage] = useState();
-    // useEffect(() => {
-    //     fetch('http://localhost:3001')
-    //         .then(res => res.text())
-    //         .then(res => {
-    //             console.log(res);
-    //             setMessage(res);
-    //         })
-    //         .catch(err => setMessage(err.message));
-    // }, []);
-    const [colours, setColours] = useState([])
+    const [percentages, setPercentages] = useState({});
+    const [movieInput, setMovieInput] = useState('');
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        // Create and establish a connection to the socketio server
+        const newSocket = io(`http://${window.location.hostname}:3000`);
+
+        setSocket(newSocket);
+
+        socket.on("receive-respose", data => {
+            setPercentages(data);
+        })
+
+        return (
+            // close the connection when the component disappears from the DOM (aka clean up the effect)
+            newSocket.close()
+        )
+    }, [setSocket]);
 
     function handleSubmit(event) {
         event.preventDefault();
-        console.log(event.target.value);
-        // TODO make a  post request
-        setColours([
-            {
-                movie: "The Avengers",
-                count: 5
-            },
-            {
-                movie: "The Bee Movie",
-                count: 10
-            },
-            {
-                movie: "Iron Man",
-                count: 1
-            }
-        ]);
+        console.log("movieInput: " + movieInput);
+        socket.emit('send-response', movieInput);
+        setMovieInput('');  // make input box empty
     }
 
     return (
         <div className="App">
+            {console.log("movies: " + JSON.stringify(percentages, null, 2))}
+            {/* {console.log("socket: " + JSON.stringify(socket, null, 2))} */}
+            <p>{socket!=null ? "Socket Connection Established" : "No connection"}</p>
+            <h1>Favourite Movie Poll</h1>
             <form onSubmit={handleSubmit}>
-                <label for="colour">Enter your favourite movie:</label>
-                <input type="text" id="colour" name="colour"></input>
+                <label htmlFor="colour">Enter your favourite movie:</label>
+                <input
+                    autoFocus
+                    value={movieInput}
+                    placeholder="Enter movie name"
+                    type="text"
+                    onChange={(e) => {
+                        setMovieInput(e.currentTarget.value);
+                    }}
+                />
                 <br></br>
-                <button type="submit" className="submit-button">Submit</button>
+                <button
+                    type="submit" 
+                    className="submit-button"
+                >
+                    Submit
+                </button>
             </form>
 
             <table>
-                <tr>
-                    <th>Colour</th>
-                    <th>Count</th>
-                </tr>
-                {colours.map((obj) => (
-                    <tr index={obj}>
-                        <td>{obj.movie}</td>
-                        <td>{obj.count}</td>
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Percentage</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {Object.keys(percentages).map((key, index) => (
+                        <tr>
+                            <td>{key}</td>
+                            <td>{percentages[key]}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     );
